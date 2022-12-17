710689d1e21923a22c2e7a6b995f0f94f79e10d5
Tamar <talphaidze.ieu2020>
2022-12-17 19:16:15 +0100

    pytest_modify

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -1,13 +1,13 @@
 from recipe_api import app
 import pytest
 
-def test_get_accounts(testing_client):
+def test_get_recipes(testing_client):
     """
     GIVEN a Flask application
-    WHEN the '/accounts' page is requested (GET)
+    WHEN the '/recipes' page is requested (GET)
     THEN check the response is valid
     """
-    response = testing_client.get('/accounts')
+    response = testing_client.get('/recipes')
     assert response.status_code == 200
 
 def test_dummy_wrong_path():
@@ -20,29 +20,29 @@ def test_dummy_wrong_path():
         response = client.get('/wrong_path')
         assert response.status_code == 404
 
-def test_create_account(testing_client):
+def test_create_recipe(testing_client):
     """
     GIVEN a Flask application
-    WHEN the '/accounts' page is posted to (POST)
+    WHEN the '/recipes' page is posted to (POST)
     THEN check the response is valid
     """
     response = testing_client.post('/recipes', json={'name': 'tiramisu', 'ingredients': 'coffee', 'rating': '5', 'favorite': 'Yes'})
     assert response.status_code == 200
 
 
-def test_update_account(testing_client):
+def test_update_recipe(testing_client):
     """
     GIVEN a Flask application
-    WHEN the '/accounts/<int:id>' page is updated (PUT)
+    WHEN the '/recipes/<int:id>' page is updated (PUT)
     THEN check the response is valid
     """
     response = testing_client.put('/recipes', json={'name': 'tiramisu', 'ingredients': 'coffee', 'rating': '5', 'favorite': 'Yes'})
     assert response.status_code == 200
 
-def test_delete_account(testing_client):
+def test_delete_recipe(testing_client):
     """
     GIVEN a Flask application
-    WHEN the '/accounts/<int:id>' page is deleted (DELETE)
+    WHEN the '/recipes/<int:id>' page is deleted (DELETE)
     THEN check the response is valid
     """
     response = testing_client.delete('/recipes')
